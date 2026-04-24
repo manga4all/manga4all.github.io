1d85a74d4975af25fe2564bbd76a530a80277cb0
manga4all <brian.ag.cd2@gmail.com>
2026-04-24 17:44:23 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
 // scripts
+import {
+  db,
+  collection,
+  getDocs
+} from "./firebase.js";
+
+const mangaGrid = document.querySelector(".manga-grid");
+
+async function loadMangas(){
+
+  const querySnapshot = await getDocs(
+    collection(db, "mangas")
+  );
+
+  querySnapshot.forEach((doc) => {
+
+    const manga = doc.data();
+
+    mangaGrid.innerHTML += `
+
+      <div class="manga-card">
+
+        <img src="${manga.cover}" alt="${manga.title}">
+
+        <div class="manga-info">
+
+          <h3>${manga.title}</h3>
+
+          <p>${manga.description}</p>
+
+          <a class="btn" href="#">
+            Leer
+          </a>
+
+        </div>
+
+      </div>
+
+    `;
+  });
+}
+
+loadMangas();
