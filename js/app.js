c5a2ab47d98469d4a4c713edffe1dcd852c36e41
manga4all <brian.ag.cd2@gmail.com>
2026-04-27 19:37:05 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,49 +7,37 @@ import {
 const mangaGrid = document.querySelector(".manga-grid");
 
 async function loadMangas(){
-
+  if(!mangaGrid) return;
   mangaGrid.innerHTML = "";
 
-  const querySnapshot = await getDocs(
-    collection(db, "mangas")
-  );
+  const querySnapshot = await getDocs(collection(db, "mangas"));
 
   querySnapshot.forEach((doc) => {
-
     const manga = doc.data();
 
-    // Limpiamos comillas dobles para que no rompan el atributo HTML del tooltip
-    const cleanDesc = manga.description.replace(/"/g, '&quot;');
+    // Verificamos si existe descripción para evitar errores
+    const rawDesc = manga.description || "Sin descripción disponible.";
+    const cleanDesc = rawDesc.replace(/"/g, '&quot;');
 
     mangaGrid.innerHTML += `
-
       <div class="manga-card" data-description="${cleanDesc}">
-
         <div class="cover-container">
-
           <img
             class="cover"
             src="${manga.cover}"
             alt="${manga.title}"
           >
-
         </div>
-
         <div class="manga-info">
-
           <h3>${manga.title}</h3>
-
           <a
             class="btn"
             href="manga.html?id=${doc.id}"
           >
             Leer
           </a>
-
         </div>
-
       </div>
-
     `;
   });
 }
